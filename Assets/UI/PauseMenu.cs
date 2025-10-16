226ef411364fb5e241dfd986a25da3f4e2fa3367
Sean Outram <outramsean0@gmail.com>
2025-10-16 15:16:24 +0100

    Some UI Work & Scaffold

---
+++ Assets/UI/PauseMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private UIDocument mainMenu;
+    [SerializeField] private UIDocument pauseMenu;
+
+    private void Awake()
+    {
+        UIShared.setInvisible(pauseMenu);
+        pauseMenu.rootVisualElement.Q<Button>("exit").clicked += UIShared.gameQuit;
+        pauseMenu.rootVisualElement.Q<Button>("exit-main").clicked += doQuitToMain;
+    }
+
+    private void doQuitToMain()
+    {
+        UIShared.setInvisible(pauseMenu);
+        UIShared.setVisible(mainMenu);
+    }
+}

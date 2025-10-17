d2fe5f7677b92ad0bdd33b54e03b59151f4764c6
Sean Outram <outramsean0@gmail.com>
2025-10-17 12:42:06 +0100

    More UI Work

--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -1,14 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private UIDocument mainMenu;
+    [SerializeField] private UIDocument settingsMenu;
 
     private void Awake()
     {
         UIShared.setVisible(mainMenu);
+        mainMenu.rootVisualElement.Q<Button>("play").clicked += switchToGame;
         mainMenu.rootVisualElement.Q<Button>("exit").clicked += UIShared.gameQuit;
+        mainMenu.rootVisualElement.Q<Button>("settings").clicked += switchToSettings;
+    }
 
+    private void switchToGame()
+    {
+        Debug.Log("Switching to Main Scene");
+        SceneManager.LoadScene("SampleScene");
+    }
+    private void switchToSettings()
+    {
+        UIShared.setInvisible(mainMenu);
+        UIShared.setVisible(settingsMenu);
     }
 }
--- Assets/UI/PauseMenu.cs
+++ Assets/UI/PauseMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine.UIElements;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private UIDocument mainMenu;
+    [SerializeField] private UIDocument settingsMenu;
     [SerializeField] private UIDocument pauseMenu;
 
     private void Awake()
@@ -11,11 +12,18 @@ public class PauseMenu : MonoBehaviour
         UIShared.setInvisible(pauseMenu);
         pauseMenu.rootVisualElement.Q<Button>("exit").clicked += UIShared.gameQuit;
         pauseMenu.rootVisualElement.Q<Button>("exit-main").clicked += doQuitToMain;
+        pauseMenu.rootVisualElement.Q<Button>("settings").clicked += switchToSettings;
     }
 
     private void doQuitToMain()
     {
         UIShared.setInvisible(pauseMenu);
         UIShared.setVisible(mainMenu);
+    }
+
+    private void switchToSettings()
+    {
+        UIShared.setInvisible(mainMenu);
+        UIShared.setVisible(settingsMenu);
     }
 }

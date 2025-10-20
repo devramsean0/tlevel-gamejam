5d1af1442685c0f97a1a6141d699c239ad3503bb
WompWompCode <sinozasineri@outlook.com>
2025-10-20 09:32:24 +0100

    Change to ball class

--- Assets/Ball/BallClass.cs
+++ Assets/Ball/BallClass.cs
@@ -3,11 +3,13 @@ public class BallClass
     public float damage;
     public float speed;
     public float size;
+    public float poisonDamage;
 
-    public BallClass(float damage, float speed, float size)
+    public BallClass(float damage, float speed, float size, float poisonMult)
     {
         this.damage = damage;
         this.speed = speed;
         this.size = size;
+        poisonDamage = damage * poisonMult;
     }
 }

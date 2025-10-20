08a9bc27207b6b6f623ee7bfd8d5616662c95885
Sean Outram <outramsean0@gmail.com>
2025-10-20 08:53:00 +0100

    Pain & Suffering (GameOver)

---
+++ Assets/UI/gameOverMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class GameOverMenu : MonoBehaviour
+{
+    [SerializeField] private UIDocument gameOverMenu;
+
+    private void Awake()
+    {
+        UIShared.setVisible(gameOverMenu);
+        gameOverMenu.rootVisualElement.Q<Button>("play").clicked += switchToGame;
+        gameOverMenu.rootVisualElement.Q<Button>("exit").clicked += UIShared.gameQuit;
+    }
+
+    private void switchToGame()
+    {
+        Debug.Log("Switching to Main Scene");
+        SceneManager.LoadScene("SampleScene");
+    }
+}
